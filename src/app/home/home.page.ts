9e6573ed40ff5d9db5369c1873cf6b6415459e12
rubenptm <ruben@audaxweb.com>
2020-11-10 11:54:10 +0000

    sample

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('slides') slides: IonSlides;
+
+  indexChanged: string='Current slide index: 0';
+  
+  slideOpts = {        
+    initialSlide: 0,
+    speed: 600,
+    spaceBetween: 20,
+    slidesPerView: 'auto',    
+    slidesOffsetBefore: 30,
+    slidesOffsetAfter: 30    
+  };
 
   constructor() {}
+
+  slideChange() {		    
+		this.slides.getActiveIndex().then(index => {      
+      this.indexChanged = 'Current slide index:'+index;
+    });
+  }
 
 }
